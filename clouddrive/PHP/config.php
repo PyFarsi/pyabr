79a277f0e9fe3adb2aafb35adad190854c908ecb
mani <manijamali2003@gmail.com>
2021-05-09 19:32:20 +0430

    Update cloud service

---
+++ clouddrive/PHP/config.php
@@ -0,0 +1,4 @@
+<?php
+define ('password','1400');     // Set your password
+define ('rootpath','stor/');    // Set your rootpath
+?>
---
+++ clouddrive/PHP/stor/readme.txt
@@ -0,0 +1,1 @@
+This is a cloud drive supported with PHP langugage
